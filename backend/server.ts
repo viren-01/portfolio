e34ebc1af527099f4f8297526c1f4aa5c4612164
viren-01 <vishwakarmavirendra74@gmail.com>
2024-05-11 02:27:27 +0530

    Adding self request for render

--- backend/server.ts
+++ backend/server.ts
@@ -1,8 +1,19 @@
 import { createServer } from 'http'
 import app from './app'
+import axios from 'axios'
 
 const port = 10000
 const server = createServer(app)
 server.listen(port, () => {
     console.log(`Server running on port ${port}`)
+
+    if (process.env.ENV === 'PROD') {
+        //self ping for render
+        setInterval(() => {
+            let url = `${process.env.HOST}:${port}/health`
+            axios.get(url).then((res) => {
+                console.log("Self Request Made...")
+            })
+        }, 840000)
+    }
 })
