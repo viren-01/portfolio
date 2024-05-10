b9d0d6e3bf01ae33aa054d9c35cce5b6f9c0a3d8
viren-01 <vishwakarmavirendra74@gmail.com>
2024-05-11 02:10:34 +0530

    changing port value

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,7 @@
 import { createServer } from 'http'
 import app from './app'
 
-const port = process.env.PORT || 1119
+const port = 10000
 const server = createServer(app)
 server.listen(port, () => {
     console.log(`Server running on port ${port}`)
