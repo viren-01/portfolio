9b76c3a8ca4f0cbbea64a25660b6e93b9e3ed175
viren-01 <vishwakarmavirendra74@gmail.com>
2024-05-11 01:58:06 +0530

    Update: updating backend build command

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,7 @@
 import { createServer } from 'http'
 import app from './app'
 
-const port = 1119
+const port = process.env.PORT || 1119
 const server = createServer(app)
 server.listen(port, () => {
     console.log(`Server running on port ${port}`)
